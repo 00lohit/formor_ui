9c5d664fcebfaed0cd45258b70da9f73d8e91f73
DnvAshok <ashok@hyposoft.in>
2021-02-23 22:45:18 +0530

    form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormComponent } from './form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'data', component: FormComponent },
+  { path: '', redirectTo: 'data', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
