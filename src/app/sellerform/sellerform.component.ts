bfdc87cfd89b2ed2ebea13c5e86466f6882cb3a5
root <root@ip-172-31-13-126.ap-south-1.compute.internal>
2021-04-09 12:43:22 +0000

    putty

---
+++ src/app/sellerform/sellerform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sellerform',
+  templateUrl: './sellerform.component.html',
+  styleUrls: ['./sellerform.component.scss']
+})
+export class SellerformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
