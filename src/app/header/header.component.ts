15468d8cdbf0dd83540b0a86906ad473be54ef58
DnvAshok <ashok@hyposoft.in>
2021-12-26 18:06:18 +0530

    origin

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -23,7 +23,7 @@ export class HeaderComponent implements OnInit {
   }
 
   onNavigate() {
-    window.location.href = 'https://farmor.ag/admin/';
+    window.location.href = `${window.location.origin}/admin/`;
   }
 
   toAbout() {
