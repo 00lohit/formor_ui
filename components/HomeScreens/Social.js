7baca13d729a7a367b0ce80119d9049925d633b6
lohit <00lohit@gmail.com>
2022-11-15 13:04:58 +0530

    changes

--- components/HomeScreens/Social.js
+++ components/HomeScreens/Social.js
@@ -68,20 +68,20 @@ export default function Social() {
         className="w-full absolute bottom-0 flex  bg-[#E1E3F9] h-36 2xl:h-40 text-sm pt-5 px-4 lg:px-16 justify-between"
         style={{ fontFamily: "Montserrat" }}
       >
-        <h4 className="text-[#575DC2] text-base 2xl:text-xl">
+        <h4 className="text-[#575DC2] text-sm lg:text-base 2xl:text-xl">
           Copyright © 2022 Farmor Agri Solutions Pvt. Ltd.
         </h4>
 
         <div className="flex">
           <a
             href="https://farmor.ag/terms"
-            className="text-[#575DC2] text-base 2xl:text-xl mx-3 underline"
+            className="text-[#575DC2] text-sm lg:text-base 2xl:text-xl mx-3 underline"
           >
             Terms & Conditions
           </a>
           <a
             href="https://farmor.ag/privacypolicy"
-            className="text-[#575DC2] text-base 2xl:text-xl mx-3 underline"
+            className="text-[#575DC2] text-sm lg:text-base 2xl:text-xl mx-3 underline"
           >
             Privacy policy
           </a>
