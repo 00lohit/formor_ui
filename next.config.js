60e8efd8b5fb81ce82b8edeed91f0929a1cb8118
mansoroshaik0201 <mansoroshaik0201@gmail.com>
2023-01-08 20:51:36 +0530

    next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  distDir: "dist"
+  useFileSystemPublicRoutes : false,
+  distDir: "dist",
+  images:{
+    unoptimized : true
+  }
 }
 
 module.exports = nextConfig
