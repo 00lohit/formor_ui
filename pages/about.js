07e22671dcd99ee29c4d7e0fe0ce0a5cb18f9fd8
Lohit <00lohit@gmail.com>
2023-10-03 15:37:11 +0530

    fix

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,18 @@
 import About from "../components/HomeScreens/About";
-import Terms from "../components/Terms";
+import close from "../public/close.svg";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function about() {
   return (
     <div>
+      <Link
+        rel="noopener noreferrer"
+        className="absolute  top-5 right-5 lg:top-10 z-50"
+        href="/"
+      >
+        <Image alt={""} src={close} />
+      </Link>
       <About />
     </div>
   );
