37b028867422fad73f636b1fe033e89e42149a4e
Lohit <00lohit@gmail.com>
2023-10-03 13:30:05 +0530

    fix

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import About from "../components/HomeScreens/About";
+import Terms from "../components/Terms";
+
+export default function about() {
+  return (
+    <div>
+      <About />
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home() {
       <Head>
         <title>FarmOR</title>
       </Head>
-      <HomeScreens></HomeScreens>
+      <HomeScreens />
     </div>
   );
 }
